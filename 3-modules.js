6e8b152dcc350c3d73725a3ae94f35f7791fde90
ashleyzam <ashleyzam1998@gmail.com>
2022-09-06 11:27:54 -0300

    built-in modules

---
+++ 3-modules.js
@@ -0,0 +1,7 @@
+const { ashley } = require("./4-firstmodules");
+const sayHi = require("./5-utils");
+const data = require("./6-alternative-flavor");
+require("./7-mind-grenade");
+// console.log(data);
+// sayHi(ashley);
+// la idea es que cada file sea un modulo.-
---
+++ nodenotes.js
@@ -0,0 +1,41 @@
+// DIFERENCIAS ENTRE BROWSER JS Y NODE JS
+// BROWSER:
+// DOM
+// WINDOW
+// INTERACTIVE APP
+// NO FILES SYSTEM
+// FRAGMENTATION
+// ES6 MODULES
+
+// NODEJS:
+// en nodeJS no tengo acceso para
+// el browser de APIs dado que no existen ya que
+// NO hay browser, entonces, no hay dom, no hay geolocalizacion ni fetch, ni todo lo relacionado a aplicaciones del navegador.
+// NO DOM
+// NO WINDOW
+// SERVER SIDE APP
+// FILESYSTEM
+// VERSION
+// COMMONJS
+
+// para evaluar codigo con node js, REPLCLI (our bestfriend) corre la app
+// a diferencia de vainilla JS.
+// REPPEL no se usa mucho.
+// Corro comando node
+// const name = "Ashley" devuelve undefined
+// si tipeo name devuelve mi nombre
+// (nada serio se escribe acá)
+// es EXACTAMENTE lo mismo q hacer un console.log(nombre) de una variable nombre.
+
+// CLI:
+// crear un proyecto en una carpeta.
+// crear un file app.js en la carpeta
+// VARIABLES GLOBALES, NO WINDOW OBJECT.-
+
+//built-in modules podemos usarlos libremente en nuestra app, cubriremos los siguientes:
+// os
+// path
+// fs
+// http
+// leer documentacion de node.
+// los built-in modules nos proveen muchas propiedades y métodos útiles para interactuar con sistemas operativos tanto como un servidor.
