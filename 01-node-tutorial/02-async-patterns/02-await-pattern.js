bd1113b912670daffb1518b36801a76fa3e5c5b8
ashleyzam <ashleyzam1998@gmail.com>
2022-09-20 11:52:34 -0300

    async / await: require().promises

--- 01-node-tutorial/02-async-patterns/02-await-pattern.js
+++ 01-node-tutorial/02-async-patterns/02-await-pattern.js
@@ -1,30 +1,23 @@
-const { readFile, writeFile } = require("fs");
-
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, "utf-8", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
-// getText("./content/first.txt")
-//   .then((result) => console.log(result))
-//   .catch((err) => console.log(err));
+const { readFile, writeFile } = require("fs").promises;
 
 const start = async () => {
   try {
-    const first = await getText("./content/first.txt");
-    const second = await getText("./content/second.txt");
+    const first = await readFile("./content/first.txt");
+    const second = await readFile("./content/second.txt");
+    await writeFile(
+      "./content/result.txt",
+      `this is awesome ${first} ${second}`,
+      { flag: "a" }
+    );
     console.log(first, second);
   } catch (error) {
     console.log(error);
   }
 };
 start();
+// getText("./content/first.txt")
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
 
 //node's option
 
